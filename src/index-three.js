0038fb5ad3975a498e4dac5b89756bf2f903a5ec
hamid814 <hamid331994@gmail.com>
2021-04-29 21:33:26 +0430

    dev

--- src/index-three.js
+++ src/index-three.js
@@ -37,6 +37,15 @@ const indexArray = [
   21,
 ];
 
+const facesArray = {
+  11: [0, 1, 2, 3, 4, 5],
+  10: [6, 7, 8, 9, 10, 11],
+  21: [12, 13, 14, 15, 16, 17],
+  20: [18, 19, 20, 21, 22, 23],
+  31: [24, 25, 26, 27, 28, 29],
+  30: [30, 31, 32, 33, 34, 35],
+};
+
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(innerWidth, innerHeight);
 renderer.setPixelRatio(Math.min(2, devicePixelRatio));
@@ -58,7 +67,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.set(0, 0, 30);
+camera.position.set(0, 0, 60);
 camera.lookAt(0, 0, 0);
 
 new OrbitControls(camera, renderer.domElement);
@@ -75,36 +84,24 @@ const light3 = new THREE.PointLight(0x886666);
 light3.position.set(0, -30, 0);
 scene.add(light3);
 
-// scene.add(new THREE.AxesHelper());
+scene.add(new THREE.AxesHelper());
 
-const cubeGeo4 = new THREE.BoxBufferGeometry(1 / 3, 1 / 3, 1 / 3);
-const cubeGeo3 = new THREE.BoxBufferGeometry();
-const cubeGeo2 = new THREE.BoxBufferGeometry(3, 3, 3);
-const cubeGeo1 = new THREE.BoxBufferGeometry(9, 9, 9);
-const cubeMat = new THREE.MeshNormalMaterial();
+const cubeGeo = new THREE.BoxBufferGeometry();
+const cubeMat = new THREE.MeshNormalMaterial({ side: 2 });
 // const cubeMat = new THREE.MeshStandardMaterial({
 //   roughness: 0.6,
 //   metalness: 0.4,
 //   // wireframe: true,
 // });
 
-const mengerWidth = 27;
+const n = 27;
 
-const menger1Pos = new V((mengerWidth / 2) * 3, 0, 0);
-const menger1 = new Menger(1, 9, menger1Pos);
-const cube1 = new THREE.Mesh(cubeGeo1, cubeMat);
+// const menger = new Menger(2);
 
-const menger2Pos = new V(mengerWidth / 2, 0, 0);
-const menger2 = new Menger(2, 3, menger2Pos);
-const cube2 = new THREE.Mesh(cubeGeo2, cubeMat);
-
-const menger3Pos = new V(mengerWidth / -2, 0, 0);
-const menger3 = new Menger(3, 1, menger3Pos);
-const cube3 = new THREE.Mesh(cubeGeo3, cubeMat);
-
-const menger4Pos = new V((mengerWidth / -2) * 3, 0, 0);
-const menger4 = new Menger(4, 1 / 3, menger4Pos);
-const cube4 = new THREE.Mesh(cubeGeo4, cubeMat);
+// const menger = new Menger(1, 1, new V(-n, 0, 0));
+// const menger2 = new Menger(2, 3, new V(0, 0, 0));
+// const menger3 = new Menger(3, 1, new V(n, 0, 0));
+// const menger4 = new Menger(4, 1 / 3, new V(n * 2, 0, 0));
 
 // 31019008
 // 1802240
@@ -113,34 +110,117 @@ const cube4 = new THREE.Mesh(cubeGeo4, cubeMat);
 // 352
 // 20
 
-const positions1 = menger1.getPositions();
-const positions2 = menger2.getPositions();
-const positions3 = menger3.getPositions();
-const positions4 = menger4.getPositions();
+function createUsingBuffer(menger) {
+  const size = menger.cubeSize;
 
-console.log(
-  'cubes Count:',
-  positions1.length + positions2.length + positions3.length + positions4.length
-);
+  const positions = menger.getPositions();
 
-function createUsignBuffer(positions, cube) {
+  console.log('faces: ', menger.facesCount);
+  console.log('cubes count:', positions.length);
+
   const bufferGeo = new THREE.BufferGeometry();
 
+  // two triangles for each face and three vertex each triangle and three ( xyz ) for each vertex
+  const positionArray = new Float32Array(menger.facesCount * 2 * 3 * 3);
+  const normalArray = new Float32Array(menger.facesCount * 2 * 3 * 3);
+
+  const positionBA = new THREE.BufferAttribute(positionArray, 3);
+  const normalBA = new THREE.BufferAttribute(normalArray, 3);
+
+  let itteration = 0;
+
+  const cube = new THREE.Mesh(cubeGeo, cubeMat);
+
+  const cubePositionAttr = cube.geometry.attributes.position;
+  const cubeNormalAttr = cube.geometry.attributes.normal;
+
+  for (let i = 0; i < positions.length; i++) {
+    const pos = positions[i].position;
+    const faces = positions[i].faces;
+
+    for (let j = 0; j < faces.length; j++) {
+      const face = faces[j];
+      const poses = facesArray[face];
+
+      for (let k = 0; k < poses.length; k++) {
+        let index = poses[k];
+        index = indexArray[index];
+
+        // size = menger.cubeSize
+        const x =
+          cubePositionAttr.array[index * 3] * size +
+          pos.x +
+          menger.centerToPosition.x;
+        const y =
+          cubePositionAttr.array[index * 3 + 1] * size +
+          pos.y +
+          menger.centerToPosition.y;
+        const z =
+          cubePositionAttr.array[index * 3 + 2] * size +
+          pos.z +
+          menger.centerToPosition.z;
+        const nx = cubeNormalAttr.array[index * 3];
+        const ny = cubeNormalAttr.array[index * 3 + 1];
+        const nz = cubeNormalAttr.array[index * 3 + 2];
+
+        positionBA.setXYZ(itteration, x, y, z);
+        normalBA.setXYZ(itteration, nx, ny, nz);
+
+        itteration++;
+      }
+    }
+  }
+
+  bufferGeo.setAttribute('position', positionBA);
+  bufferGeo.setAttribute('normal', normalBA);
+
+  // bufferGeo.drawRange.count = 3;
+  console.log(bufferGeo);
+
+  const mesh = new THREE.Mesh(bufferGeo, cubeMat);
+
+  scene.add(mesh);
+}
+
+function createUsingStatic() {
+  const level = 2;
+  const width = Math.pow(3, level);
+  const positions = [];
+
+  for (let i = 1; i <= width; i++) {
+    for (let j = 1; j <= width; j++) {
+      for (let k = 1; k <= width; k++) {
+        const v = new V(i, j, k);
+
+        const isInMenger = isCube(v, width);
+
+        isInMenger && positions.push(v);
+      }
+    }
+  }
+
+  const bufferGeo = new THREE.BufferGeometry();
+
+  // two triangles for each face and three vertex each triangle and three ( xyz ) for each vertex
   const positionArray = new Float32Array(positions.length * 108);
   const normalArray = new Float32Array(positions.length * 108);
 
   const positionBA = new THREE.BufferAttribute(positionArray, 3);
   const normalBA = new THREE.BufferAttribute(normalArray, 3);
 
+  let itteration = 0;
+
+  const cube = new THREE.Mesh(cubeGeo, cubeMat);
+
+  const cubePositionAttr = cube.geometry.attributes.position;
+  const cubeNormalAttr = cube.geometry.attributes.normal;
+
   let num = 0;
 
   for (let i = 0; i < positions.length; i++) {
+    num++;
+
     const pos = positions[i];
-
-    cube.position.set(pos.x, pos.y, pos.z);
-
-    const cubePositionAttr = cube.geometry.attributes.position;
-    const cubeNormalAttr = cube.geometry.attributes.normal;
 
     for (let j = 0; j < indexArray.length; j++) {
       const index = indexArray[j];
@@ -153,10 +233,14 @@ function createUsignBuffer(positions, cube) {
       const ny = cubeNormalAttr.array[index * 3 + 1];
       const nz = cubeNormalAttr.array[index * 3 + 2];
 
-      positionBA.setXYZ(j + i * 36, x, y, z);
-      normalBA.setXYZ(j + i * 36, nx, ny, nz);
+      positionBA.setXYZ(itteration, x, y, z);
+      normalBA.setXYZ(itteration, nx, ny, nz);
+
+      itteration++;
     }
   }
+
+  console.log(num);
 
   bufferGeo.setAttribute('position', positionBA);
   bufferGeo.setAttribute('normal', normalBA);
@@ -185,16 +269,19 @@ function render() {
 }
 
 async function startApp() {
-  await createUsignBuffer(positions1, cube1);
-  await createUsignBuffer(positions2, cube2);
-  await createUsignBuffer(positions3, cube3);
-  await createUsignBuffer(positions4, cube4);
-  // await createStandard();
+  // createUsingBuffer(menger);
+  // createStandard();
+  createUsingStatic();
 
   render();
 
   setInterval(() => {
-    console.log('render calls:', renderer.info.render.calls);
+    console.log(
+      'render calls:',
+      renderer.info.render.calls,
+      'tiangles: ',
+      renderer.info.render.triangles
+    );
   }, 1000);
 }
 
--- src/index.js
+++ src/index.js
@@ -84,18 +84,82 @@ const getEdgeDirection = (type, index, edgeDirection) => {
   }
 };
 
+const isCube = (v, width, log) => {
+  /**
+   * input:
+   * v: type: class V
+   * width: width of the menger, a power of 3
+   *
+   * outputs:
+   * 1: means there is a cube at position
+   * 0: means there is not a cube at position
+   * 2: position is out of menger
+   */
+
+  function getLinear(n, w) {
+    let currentWidth = w || width;
+    const mod = n % currentWidth;
+
+    if (mod > currentWidth / 3 && mod <= currentWidth - currentWidth / 3) {
+      return 1;
+    } else if (currentWidth !== 3) {
+      return getLinear(n, currentWidth / 3);
+    } else {
+      return 0;
+    }
+  }
+
+  if (
+    v.x > this.width ||
+    v.x <= 0 ||
+    v.y > this.width ||
+    v.y <= 0 ||
+    v.z > this.width ||
+    v.z <= 0
+  ) {
+    return 2;
+  }
+
+  const x = getLinear(v.x);
+  const y = getLinear(v.y);
+  const z = getLinear(v.z);
+
+  log && console.log(x, y, z);
+
+  if (x + y + z >= 3) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
 class Menger {
-  constructor(level, cubeSize, position, isMother, isEdge, direction) {
+  constructor(
+    level = 1,
+    cubeSize = 1,
+    position = new V(0, 0, 0),
+    mother = undefined,
+    isEdge = undefined,
+    direction = undefined
+  ) {
     this.level = level;
-    this.position = position;
+    this.position = position || new V(0, 0, 0);
     this.cubeSize = cubeSize;
     this.width = cubeSize * Math.pow(3, level);
     this.children = [];
     this.childSize = this.width / 3;
     this.childrenType = null;
-    this.isMother = isMother === undefined ? true : isMother;
+    this.mother = mother;
     this.isEdge = isEdge === undefined ? false : isEdge;
     this.direction = direction;
+    this.center = new V(
+      this.width / 2 + 0.5,
+      this.width / 2 + 0.5,
+      this.width / 2 + 0.5
+    );
+    this.centerToPosition = this.position.retsub(this.center);
+
+    this.facesCount = 0;
 
     if (level === 1) {
       this.childrenType = 'cube';
@@ -106,21 +170,76 @@ class Menger {
     this.initChildren();
   }
 
+  isNeighbour(v, log) {
+    if (log) {
+      console.log(v);
+    }
+
+    /**
+     * input:
+     * v: type: class V
+     *
+     * outputs:
+     * 1: means there is a cube at position
+     * 0: means there is not a cube at position
+     * 2: position is out of menger
+     */
+
+    // const width = 3;
+    // this.width = width;
+    const width = this.level * 3;
+
+    function getLinear(n, w) {
+      let currentWidth = w || width;
+      const mod = n % currentWidth;
+
+      if (mod > currentWidth / 3 && mod <= currentWidth - currentWidth / 3) {
+        return 1;
+      } else if (currentWidth !== 3) {
+        return getLinear(n, currentWidth / 3);
+      } else {
+        return 0;
+      }
+    }
+
+    if (
+      v.x > this.width ||
+      v.x <= 0 ||
+      v.y > this.width ||
+      v.y <= 0 ||
+      v.z > this.width ||
+      v.z <= 0
+    ) {
+      return 2;
+    }
+
+    const x = getLinear(v.x);
+    const y = getLinear(v.y);
+    const z = getLinear(v.z);
+
+    if (x + y + z >= 2) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
   initChildren() {
-    if (this.isMother) {
+    if (!this.mother) {
+      // this is the mother menger
       let positions = [];
 
-      const xP = this.position.x + this.childSize;
-      const xM = this.position.x - this.childSize;
-      const yP = this.position.y + this.childSize;
-      const yM = this.position.y - this.childSize;
-      const zP = this.position.z + this.childSize;
-      const zM = this.position.z - this.childSize;
+      const xP = this.center.x + this.childSize;
+      const xM = this.center.x - this.childSize;
+      const yP = this.center.y + this.childSize;
+      const yM = this.center.y - this.childSize;
+      const zP = this.center.z + this.childSize;
+      const zM = this.center.z - this.childSize;
 
       // default positoins
-      const dX = this.position.x;
-      const dY = this.position.y;
-      const dZ = this.position.z;
+      const dX = this.center.x;
+      const dY = this.center.y;
+      const dZ = this.center.z;
 
       positions[0] = new V(dX, yP, zM);
       positions[1] = new V(xP, yP, zM);
@@ -146,8 +265,32 @@ class Menger {
       positions[19] = new V(xM, yM, zM);
 
       if (this.childrenType === 'cube') {
-        positions.forEach((pos) => {
-          const child = new Cube(pos, this.childSize);
+        positions.forEach((pos, posId) => {
+          const faces = [];
+
+          for (let i = 0; i < facesIds.length; i++) {
+            const face = facesIds[i];
+            const facePos = face.position;
+
+            const neighbourPos = new V(pos.x, pos.y, pos.z);
+            neighbourPos.x += facePos.x;
+            neighbourPos.y += facePos.y;
+            neighbourPos.z += facePos.z;
+
+            const log = posId === 2;
+
+            log && console.error(1);
+            log && console.log(pos);
+
+            if (this.isNeighbour(neighbourPos, log)) {
+              faces.push(face.id);
+              log && console.log(face.id);
+            }
+          }
+
+          this.addToFacesCount(faces.length);
+
+          const child = new Cube(pos, this.childSize, faces);
 
           this.children.push(child);
         });
@@ -166,7 +309,7 @@ class Menger {
             this.level - 1,
             this.cubeSize,
             pos,
-            false,
+            this,
             isEdge,
             direction
           );
@@ -175,6 +318,7 @@ class Menger {
         });
       }
     } else {
+      // this is not the top level menger ( mother menger )
       let positions = [];
 
       // posiotns minus and plus one cubeSize
@@ -191,8 +335,6 @@ class Menger {
       const dZ = this.position.z;
 
       if (!this.isEdge) {
-        // is not an edge
-
         positions[0] = new V(dX, yP, zM);
         positions[1] = new V(xP, yP, zM);
         positions[2] = new V(xP, yP, dZ);
@@ -216,7 +358,7 @@ class Menger {
         positions[18] = new V(xM, yM, dZ);
         positions[19] = new V(xM, yM, zM);
       } else {
-        // is an edge
+        // this child is an edge menger
         if (this.direction === x) {
           positions = [
             new V(dX, yP, dZ),
@@ -284,8 +426,26 @@ class Menger {
       }
       if (!this.isEdge) {
         if (this.childrenType === 'cube') {
-          positions.forEach((pos) => {
-            const child = new Cube(pos, this.childSize);
+          positions.forEach((pos, posId) => {
+            const faces = [];
+
+            for (let i = 0; i < facesIds.length; i++) {
+              const face = facesIds[i];
+              const facePos = face.position;
+
+              const neighbourPos = new V(pos.x, pos.y, pos.z);
+              neighbourPos.x += facePos.x;
+              neighbourPos.y += facePos.y;
+              neighbourPos.z += facePos.z;
+
+              if (this.mother.isNeighbour(neighbourPos)) {
+                faces.push(face.id);
+              }
+            }
+
+            this.mother.addToFacesCount(faces.length);
+
+            const child = new Cube(pos, this.childSize, faces);
 
             this.children.push(child);
           });
@@ -304,7 +464,7 @@ class Menger {
               this.level - 1,
               this.cubeSize,
               pos,
-              false,
+              this.mother,
               isEdge,
               direction
             );
@@ -313,10 +473,28 @@ class Menger {
           });
         }
       } else {
-        // this is an edge menger
+        // this child is an edge menger
         if (this.childrenType === 'cube') {
           positions.forEach((pos) => {
-            const child = new Cube(pos, this.childSize);
+            const faces = [];
+
+            for (let i = 0; i < facesIds.length; i++) {
+              const face = facesIds[i];
+              const facePos = face.position;
+
+              const neighbourPos = new V(pos.x, pos.y, pos.z);
+              neighbourPos.x += facePos.x;
+              neighbourPos.y += facePos.y;
+              neighbourPos.z += facePos.z;
+
+              if (this.mother.isNeighbour(neighbourPos)) {
+                faces.push(face.id);
+              }
+            }
+
+            this.mother.addToFacesCount(faces.length);
+
+            const child = new Cube(pos, this.childSize, faces);
 
             this.children.push(child);
           });
@@ -333,7 +511,7 @@ class Menger {
               this.level - 1,
               this.cubeSize,
               pos,
-              false,
+              this.mother,
               isEdge,
               direction
             );
@@ -345,8 +523,8 @@ class Menger {
     }
   }
 
-  setIsEdge(state) {
-    this.isEdge = state;
+  addToFacesCount(number) {
+    this.facesCount += number;
   }
 
   getPositions() {
@@ -354,7 +532,17 @@ class Menger {
 
     if (this.childrenType === 'cube') {
       this.children.forEach((cube) => {
-        list.push(cube.position);
+        const centerToPosition = this.mother
+          ? this.mother.centerToPosition
+          : this.centerToPosition;
+
+        // const position = cube.position.retadd(centerToPosition);
+        const position = cube.position;
+
+        list.push({
+          position,
+          faces: cube.faces,
+        });
       });
 
       return list;
@@ -378,9 +566,21 @@ class Menger {
 }
 
 class Cube {
-  constructor(position, size) {
+  constructor(position, size, faces) {
+    /**
+     * faces is array of numbers wehre:
+     * 11 is for x direction positive face
+     * 10 is for x direction negative face
+     * 21 is for y direction positive face
+     * 20 is for y direction negative face
+     * 31 is for z direction positive face
+     * 30 is for z direction negative face
+     */
+
     this.position = position;
+    this.index = position;
     this.size = size;
+    this.faces = faces;
   }
 
   drawP5() {
@@ -398,16 +598,16 @@ class V {
     this.z = z;
   }
 
-  sub(vec) {
-    this.x -= vec.x;
-    this.y -= vec.y;
-    this.z -= vec.z;
-  }
-
   add(vec) {
     this.x += vec.x;
     this.y += vec.y;
     this.z += vec.z;
+  }
+
+  sub(vec) {
+    this.x -= vec.x;
+    this.y -= vec.y;
+    this.z -= vec.z;
   }
 
   mul(vec) {
@@ -422,10 +622,18 @@ class V {
     this.z /= vec.z;
   }
 
-  retsub(vec) {
+  retsub(xOrV, y, z) {
     const newV = new V(this.x, this.y, this.z);
 
-    newV.sub(vec);
+    if (y === undefined && z === undefined) {
+      // a vector is passed in args
+      newV.sub(xOrV);
+    } else {
+      // x, y ,z are passed in args
+      const subV = new V(xOrV, y, z);
+
+      newV.sub(subV);
+    }
 
     return newV;
   }
@@ -436,7 +644,7 @@ class V {
     if (y === undefined && z === undefined) {
       // a vector is passed is args
 
-      newV.add(vec);
+      newV.add(xOrV);
     } else {
       // x, y ,z are passed in args
 
@@ -464,3 +672,31 @@ class V {
     return newV;
   }
 }
+
+// right, left, top, bottom, front, back
+const facesIds = [
+  {
+    position: new V(+1, 0, 0),
+    id: 11,
+  },
+  {
+    position: new V(-1, 0, 0),
+    id: 10,
+  },
+  {
+    position: new V(0, +1, 0),
+    id: 21,
+  },
+  {
+    position: new V(0, -1, 0),
+    id: 20,
+  },
+  {
+    position: new V(0, 0, +1),
+    id: 31,
+  },
+  {
+    position: new V(0, 0, -1),
+    id: 30,
+  },
+];
